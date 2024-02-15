c035e43e46e9cfd07629b3c348988745e739d5a5
Bastian Bastias Sanchez <bastian.bastias@gmail.com>
2024-02-14 22:05:36 -0500

    added Student v2

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Subscribers from './Subscribers';
 import Students from './Students';
+import Students2 from './Students2';
 
 function App() {
   return (
-    <Students />
+    <Students2 />
   );
 }
 
